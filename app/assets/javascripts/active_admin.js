3008ded46953a0fda5372202c99b654ee80781a1
Frederik Spang <frederik@progras.dk>
2019-08-13 20:43:46 +0200

    Theming

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,11 +1,11 @@
 //= require active_admin/base
 //= require array-input
 //= require tinymce
+//= require tinymce/themes/silver/theme
 
 $(document).ready(function() {
   tinyMCE.init({
     selector: '.tinymce-input',
-    theme: 'silver',
     menubar: false
   });
 });
